5d0cf08036cf91bb26cde97c97f0cd066e3b2786
silviuhogas <silviu.hogas@yahoo.com>
2024-03-09 21:19:04 +0200

    fragments for login/register
    default fragment is login fragment, added navigation to register fragment when pressing "not yet registered" text

--- app/src/main/java/com/example/proiectandroidmaster/LoginPage.kt
+++ app/src/main/java/com/example/proiectandroidmaster/LoginPage.kt
@@ -1,24 +1,37 @@
 package com.example.proiectandroidmaster
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.proiectandroidmaster.databinding.FragmentLoginPageBinding
 
 class LoginPage : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentLoginPageBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_page, container, false)
+    ): View {
+        _binding = FragmentLoginPageBinding.inflate(inflater, container, false)
+
+        binding.transferToRegisterText.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main, RegisterPage())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
