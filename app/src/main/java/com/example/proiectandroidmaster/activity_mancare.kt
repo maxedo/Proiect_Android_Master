5bccf7afd0238a5c9d3ccbef34d0951da88626d2
maxedo <maxitiansergiu@gmail.com>
2024-06-15 15:56:12 +0300

    another ui change

--- app/src/main/java/com/example/proiectandroidmaster/activity_mancare.kt
+++ app/src/main/java/com/example/proiectandroidmaster/activity_mancare.kt
@@ -5,7 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import android.widget.Spinner
+import android.widget.ArrayAdapter
 class activity_mancare : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +17,16 @@ class activity_mancare : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val unitSpinner: Spinner = findViewById(R.id.unit_spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.units_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            unitSpinner.adapter = adapter
+        }
+
     }
 }
